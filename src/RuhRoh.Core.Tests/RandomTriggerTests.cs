d5c0512fbf5765fa085207dffb8e6aaac2288e15
Wesley Cabus <wesley.cabus@realdolmen.com>
2017-06-05 19:04:39 +0200

    Added tests for Random trigger

---
+++ src/RuhRoh.Core.Tests/RandomTriggerTests.cs
@@ -0,0 +1,55 @@
+﻿using RuhRoh.Core.Triggers;
+using Xunit;
+
+namespace RuhRoh.Core.Tests
+{
+    public class RandomTriggerTests
+    {
+        [Theory]
+        [InlineData(0.5)]
+        [InlineData(0.51)]
+        [InlineData(0.5000000001)]
+        [InlineData(0.6)]
+        [InlineData(0.9999999999999999999999)]
+        public void Random_Should_Trigger_When_Randomizer_Returns_Zero_Point_Five_Or_Higher(double rnd)
+        {
+            var randomizer = new TestRandomizer(rnd);
+            var t = new Random(randomizer);
+
+            var result = t.WillAffect();
+
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(0.0000000000001)]
+        [InlineData(0.23435345)]
+        [InlineData(0.3)]
+        [InlineData(0.499999999999999)]
+        public void Random_Should_Not_Trigger_When_Randomizer_Returns_Less_Than_Zero_Point_Five(double rnd)
+        {
+            var randomizer = new TestRandomizer(rnd);
+            var t = new Random(randomizer);
+
+            var result = t.WillAffect();
+
+            Assert.False(result);
+        }
+
+        private class TestRandomizer : IRandomizer
+        {
+            private readonly double _next;
+
+            public TestRandomizer(double next)
+            {
+                _next = next;
+            }
+
+            public double Next()
+            {
+                return _next;
+            }
+        }
+    }
+}
